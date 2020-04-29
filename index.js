a603fbafde17ad5138f2b390d5e0064e30d441b0
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2020-04-29 19:21:33 +0100

    Actual drop of listener naughtiness

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const fetch = require('isomorphic-fetch');
 const session = require('telegraf/session');
 const https = require('https');
 const fs = require('fs');
+const request = require('request');
 
-// Pull in Imports
-const telegramToken = process.env.TELEGRAM_TOKEN;
+// Pull in ENVs
+const telegramToken = process.env.NANNYBOT;
+const healthCheckURL = process.env.NANNYHEALTHCHECK;
+
+// Define the bot
 const bot = new Telegraf(telegramToken);
 
 // Use Telegraf Session
@@ -21,34 +25,69 @@ bot.use((ctx, next) =>
   return next().then(() => 
   {
     const elapsed = new Date() - start;
-        console.log('response time %sms', elapsed);
+    console.log('[NannyBot] Saw message in %sms', elapsed);
   });
 });
 
-// Process messages
-bot.hears(/^nominate$/i, (ctx) => 
+// Global message handler
+bot.on('text', ctx =>
 {
-  if( !ctx.message.reply_to_message ) 
+  const messager = ctx.message.from.first_name + " " + ctx.message.from.last_name;
+  ctx.replyWithMarkdown('Saw message "' + ctx.message.text + '" from (' + messager + ')');
+  if( ctx.message.text.indexOf( "nanny") != -1 || 
+      ctx.message.text.indexOf( "Nanny") != -1 )
   {
-    ctx.replyWithMarkdown(`Please reply to the message you want to nominate`);
-  } 
-  else 
-  {
-    data = 
-    {
-      nominee: ctx.message.reply_to_message.from.first_name + " " + ctx.message.reply_to_message.from.last_name,
-      message: ctx.message.reply_to_message.text,
-      nominator: ctx.message.from.first_name + " " + ctx.message.from.last_name,
-      date: new Date().toString(),
-      channel: ctx.message.chat.title,
-      linkToGif: "N/A",
-      linkToPhoto: "N/A"
-    };
-            
-    ctx.replyWithMarkdown(`Message received`);
-            
-    console.log("Message the was nominated was " + ctx.message.reply_to_message.text + "\nNominated By " + ctx.message.reply_to_message.from.first_name + " " + ctx.message.reply_to_message.from.last_name);
+    ctx.replyWithMarkdown('  Saw mention of self' );
   }
+
+  if( ctx.message.text.indexOf('healthcheck') != -1 )
+  {
+    var target = "http://nannybotbackend-botbackend.apps.conroe.demolab.local/api/health/check";
+    request.get(target, (error, response, body) =>
+    {
+      if( error )
+      {
+        ctx.replyWithMarkdown("NannyBot backend reports an error.");
+        return;
+      }
+
+      if( response.statusCode == 200 )
+      {
+        ctx.replyWithMarkdown("NannyBot backend reports healthy.");
+      }
+      else
+      {
+        ctx.replyWithMarkdown('NannyBot backend reports unhealthy (' + response.statusCode + ')');
+      }
+    });
+  }
+
+  if( ctx.message.text.indexOf( "murphy" ) != -1 )
+  {
+    var target = "http://meowworld-sandbox.e8ca.engint.openshiftapps.com/cat/murphy"; 
+    request.get(target, (error, response, body) =>
+    {
+      if( error ) 
+      {
+        ctx.replyWithMarkdown("Unable to reference Webservice");
+        return;
+      }
+
+      ctx.replyWithMarkdown("NannyBot called " + target );
+      ctx.replyWithMarkdown("Webservice response: " + body);
+    });
+  }
+});
+
+// Process messages
+bot.hears(/^(N|n)anny*/i, (ctx) =>
+{
+  ctx.replyWithMarkdown('Heard ' + ctx.message);
+});
+
+bot.hears(/^TEST$/i, (ctx) => 
+{
+  ctx.replyWithMarkdown('Heard fixed term TEST from ' + ctx.message.from.last_name + "," + ctx.message.from.first_name );
 });
 
 bot.startPolling();
